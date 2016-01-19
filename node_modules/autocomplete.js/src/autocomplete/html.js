0ab194632ea4179e03a68f2dd23aa03669eb1604
Dario Vladovic <d.vladimyr@gmail.com>
2016-01-19 10:04:10 +0100

    Removing node_modules

--- node_modules/autocomplete.js/src/autocomplete/html.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-module.exports = {
-  wrapper: '<span class="algolia-autocomplete"></span>',
-  dropdown: '<span class="aa-dropdown-menu"></span>',
-  dataset: '<div class="aa-dataset-%CLASS%"></div>',
-  suggestions: '<span class="aa-suggestions"></span>',
-  suggestion: '<div class="aa-suggestion"></div>'
-};
